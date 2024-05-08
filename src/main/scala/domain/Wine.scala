1c6d25f31a1b41df1ecdd529abe5bcfe9e72b2e2
Nathan Knox <nathan.knox@gmail.com>
2024-05-08 01:15:46 -0400

    working generators

--- src/main/scala/domain/Wine.scala
+++ src/main/scala/domain/Wine.scala
@@ -2,24 +2,50 @@ package domain
 
 import zio.Chunk
 import domain.BottleLayout.Bottle
+import zio.schema.Schema
+import zio.schema.DeriveSchema
+import zio.schema.DeriveGen
+import zio.test.{Gen, Sized}
 
-type WineInfo
-type ShelfDescription = String
-
-sealed trait BottleDetails
-object BottleDetails {
-  case object None extends BottleDetails
+sealed trait WineInfo
+object WineInfo {
+  case object WineInfoPlaceholder extends WineInfo
 }
+sealed trait ShelfDescription
+object ShelfDescription {
+  case object ShelfDescriptionPlaceholder extends ShelfDescription
+}
+
 sealed trait BottleLayout
 object BottleLayout {
+  implicit val schema: Schema[BottleLayout] = DeriveSchema.gen
+  val gen: Gen[Sized, BottleLayout]         = DeriveGen.gen
+
   case object None                                                             extends BottleLayout
   final case class SideBySide(left: BottleLayout, right: BottleLayout)         extends BottleLayout
   final case class Stacked(top: BottleLayout, bottom: BottleLayout)            extends BottleLayout
   final case class FrontToBack(front: BottleLayout, back: BottleLayout)        extends BottleLayout
   final case class Shelf(contents: Chunk[BottleLayout], details: ShelfDetails) extends BottleLayout
   final case class Bottle(details: BottleDetails)                              extends BottleLayout
+  object Bottle {
+    implicit val schema: Schema[Bottle] = DeriveSchema.gen
+    val gen                             = DeriveGen.gen
+  }
+}
+
+sealed trait BottleDetails
+object BottleDetails {
+  implicit val schema: Schema[BottleDetails] = DeriveSchema.gen
+  val gen                                    = DeriveGen.gen
+  case object BottlePlaceholder extends BottleDetails
 }
 
 final case class ShelfDetails(capapcity: Int, description: Option[ShelfDescription] = None)
+object ShelfDetails {
+  implicit val schema: Schema[ShelfDetails] = DeriveSchema.gen
+}
 
 final case class WineLabel(info: WineInfo)
+object WineLabel {
+  implicit val schema: Schema[WineLabel] = DeriveSchema.gen
+}
--- src/test/scala/domain/WineSpec.scala
+++ src/test/scala/domain/WineSpec.scala
@@ -3,16 +3,48 @@ package domain
 import zio.test._
 import zio.test.TestAspect._
 import zio.test.Assertion._
-import zio._
+import zio.ZIO
+import zio.Chunk
+import domain.BottleLayout._
+import zio.Duration
+import zio.durationInt
 
 object WineSpec extends ZIOSpecDefault {
+  val testIntGen          = Gen.int(0, 100)
+  val testBottleLayoutGen = BottleLayout.gen
+  val testBottleGen       = Bottle.gen
   override def spec = suite("WineSpec")(
+    test("a Shelf can hold a Bottle") {
+      val bottle: Bottle = Bottle(BottleDetails.BottlePlaceholder)
+      val shelf: Shelf   = Shelf(Chunk(bottle), ShelfDetails(capapcity = 1))
+      assertTrue(true)
+    } @@ timed,
+    test("can Gen some Ints") {
+      check(testIntGen) { n =>
+        assertTrue {
+          //println(s"an INT: $n")
+          n + n == 2 * n
+        }
+      }
+    } @@ timed, // @@ samples(5),
+    test("can Gen some Bottles") {
+      check(testBottleGen) { bottle =>
+        println(s"a Bottle: ${bottle.toString()}")
+        assertTrue(true)
+      }
+    } @@ samples(5) @@ timed,
+    test("can Gen some BottleLayouts") {
+      check(testBottleLayoutGen) { bottleLayout =>
+        println(s"a BottleLayout: ${bottleLayout.toString()}")
+        assertTrue(true)
+      }
+    } @@ ignore, // @@ samples(5) @@ timeout(Duration.fromSeconds(5)) @@ timed,
     test("exampleTest") {
       val program = for {
         result <- ZIO.fail(new RuntimeException("NOT YET IMPLEMENTED"))
       } yield assertTrue(1 == 1)
 
       program
-    } // @@ failing
-  ).provide()
+    } @@ timed @@ failing
+  )
 }
